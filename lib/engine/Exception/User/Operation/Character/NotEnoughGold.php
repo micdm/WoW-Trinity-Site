c2d07b16d8391aecfeccf69cd51b8cb310c2075a
Mic <mic-dm@mail.ru>
2011-07-30 00:07:09 +0700

    new: потерявшиеся директории + исключение

---
+++ lib/engine/Exception/User/Operation/Character/NotEnoughGold.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * У персонажа недостаточно золота.
+ * @package Exception_User_Operation_Character
+ * @author Mic, 2010
+ */
+class Exception_User_Operation_Character_NotEnoughGold extends Exception_User_Operation_Character_Base {
+	
+};
+
